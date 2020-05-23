1ff7674d9cfd8c2948a34effc18906ae524fcda6
div-antk <51283468+owaii@users.noreply.github.com>
2020-05-23 22:57:44 +0900

    プロパティの宣言

--- app/Service/RestaurantBubbleBuilder.php
+++ app/Service/RestaurantBubbleBuilder.php
@@ -6,7 +6,17 @@ use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder;
 
 class RestaurantBubbleBuilder implements ContainerBuilder
 {
-
+  // プロパティの宣言
+  private $imageUrl;
+  private $name;
+  private $closestStation;
+  private $minutesByFoot;
+  private $category;
+  private $budget;
+  private $latitude;
+  private $longitude;
+  private $phoneNumber;
+  private $restaurantUrl;
 }
 
 ?>
